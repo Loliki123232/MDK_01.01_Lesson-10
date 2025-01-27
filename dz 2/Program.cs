843edef83b8be2e1e3aa7cb7ae767d3a41d8725d
Кирилл <@DESKTOP-7V2T56C>
2025-01-27 22:03:53 +0300

    Добавьте файлы проекта.

---
+++ dz 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_2
+{
+    public abstract class Translator
+    {
+        public abstract string Translate(string text);
+        public abstract string getLanguage();
+
+        public void tap()
+        {
+            Console.WriteLine("Я переводчик");
+        }
+    }
+
+
+    public class EnglishTranslator : Translator
+    {
+        public override string Translate(string text)
+        {
+           
+            return $"Перевод с английского: {text}";
+        }
+
+        public override string getLanguage()
+        {
+            return "Я переводчик с английского";
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EnglishTranslator translator = new EnglishTranslator();
+            translator.tap();
+            Console.WriteLine(translator.getLanguage());
+            Console.WriteLine(translator.Translate("Hello"));
+
+            Console.ReadKey();
+        }
+    }
+}
